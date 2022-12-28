2f7dc66d20cc59aa94b14ee48e29caa4a81e87c6
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2022-12-28 11:57:50 +0100

    feat(music): add triads

--- music/harmony/index.js
+++ music/harmony/index.js
@@ -2,13 +2,16 @@
 
 import parseArgs from "minimist"
 import njk from "nunjucks"
+const { Environment, FileSystemLoader } = njk
 const { render } = njk
 import { $ } from "zx"
 
 const argsConfig = { default: { f: "pdf" } }
 const args = parseArgs(process.argv.slice(2), argsConfig)
 const source = args._[0].replace(/\.lys$/, "")
-const score = render(`${source}.lys`)
+const tags = { commentStart: "<#", commentEnd: "#>" }
+const env = new Environment(new FileSystemLoader("."), { tags })
+const score = env.render(`${source}.lys`)
 const lilypond = $`lilypond -f ${args.f} -o ${source} -`
 lilypond.stdin.write(score)
 lilypond.stdin.end()
