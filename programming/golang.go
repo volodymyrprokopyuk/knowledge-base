7b8072c4de0b0fa1fbf55f20c2a9e29bb4b942ea
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2024-01-22 23:22:02 +0100

    feat(go): extend Go docs

--- programming/golang.go
+++ programming/golang.go
@@ -46,74 +46,75 @@
 // for i, r := range []rune(s) { fmt.Printf("%d: %c, ", i, r) }
 // string([]rune(s)[:2]) // first two runes
 
-/* Arrays */
-// allocated on stack as array size is known at compile-time
-var a [3]int // [0 0 0] automatic zero initialization
-// explicit initialization
-b, c := [3]int{}, [...]int{1, 2, 3} // [0 0 0], [1 2 3]
-d := [...]int{1, 2: 2, 4: 3} // [1 0 2 0 3] sparse array
+// /* Arrays */
+// // allocated on stack as array size is known at compile-time
+// var a [3]int // [0 0 0] automatic zero initialization
+// // explicit initialization
+// b, c := [3]int{}, [...]int{1, 2, 3} // [0 0 0], [1 2 3]
+// d := [...]int{1, 2: 2, 4: 3} // [1 0 2 0 3] sparse array
 
-/* Slices */
-// slice = pointer to an array + len + cap; dynamic array
-// slice = multiple views onto an array; update, but not append
-// slice = single view per array; update and append
-// slices are not comparable
-var (a []int, b []int(nil)) // nil slice without allocation => nil
-a = append(a, 1, 2) // [1 2] increments length, dynamic reallocation
-b := []int{1, 2, 3} // [1 2 3] slice initialization
-c := []int{1, 2: 2, 4: 3} // [1 0 2 0 3] sparse slice
-d := make([]int, 3) // [0 0 0] zero initialized slice with capacity 3
-e, f := []int{}, make([]int, 0, 3) // allocated empty slice len = 0, cap 3
-a := [...]int{1, 2, 3}
-var b []int = a[:] // array => slice, shared memory
-var c []int = make([]int, len(a))
-copy(c, a[:]) // array => slice, separate copies; destination must be init
-// [i:j:cap] = [i, j), cap limits side effects by reallocating when len == cap
-// make a slice copy instead of slicing big arrays to dispose unused memory
-for i, v := range a { fmt.Println(i, v) }
+// /* Slices */
+// // slice = pointer to an array + len + cap; dynamic array
+// // slice = multiple views onto an array; update, but not append
+// // slice = single view per array; update and append
+// // slices are not comparable
+// var (a []int, b []int(nil)) // nil slice without allocation => nil
+// a = append(a, 1, 2) // [1 2] increments length, dynamic reallocation
+// b := []int{1, 2, 3} // [1 2 3] slice initialization
+// c := []int{1, 2: 2, 4: 3} // [1 0 2 0 3] sparse slice
+// d := make([]int, 3) // [0 0 0] zero initialized slice with capacity 3
+// e, f := []int{}, make([]int, 0, 3) // allocated empty slice len = 0, cap 3
+// a := [...]int{1, 2, 3}
+// var b []int = a[:] // array => slice, shared memory
+// var c []int = make([]int, len(a))
+// copy(c, a[:]) // array => slice, separate copies; destination must be init
+// // [i:j:cap] = [i, j), cap limits side effects by reallocating when len == cap
+// // make a slice copy instead of slicing big arrays to dispose unused memory
+// for i, v := range a { fmt.Println(i, v) }
 
-/* Maps */
-// maps are not comparable
-var a = map[string]int{} // empty map
-var b = map[string]int{"a": 1, "b": 2} // initialized map
-var c = make(map[string]int, 3) // map with capacity
-c["a"] = 1
-if v, ok := c["a"]; ok { fmt.Println(v) }
-for k, v := range b { fmt.Println(k, v) }
-delete(c, "a") // maps never shrink, even after deletion buckets are not removed
+// /* Maps */
+// // maps are not comparable
+// var a = map[string]int{} // empty map
+// var b = map[string]int{"a": 1, "b": 2} // initialized map
+// var c = make(map[string]int, 3) // map with capacity
+// c["a"] = 1
+// if v, ok := c["a"]; ok { fmt.Println(v) }
+// for k, v := range b { fmt.Println(k, v) }
+// delete(c, "a") // maps never shrink, even after deletion buckets are not removed
 
-/* Structs, types */
-// nominal typing, block-level scope
-  var a, b Bayan = Bayan{model: "Nextra", year: 2022}, Bayan{"Omnia", 2023}
-  var c, d *Bayan = new(Bayan), &Bayan{"Selecta", 2024}
-  fmt.Println(d.model) // (*d).model => d.model
-  e := []Bayan{{model: "Nextra", year: 2022}, {"Omnia", 2023}}
-// constructor function = correct and uniform initialization of structs
-// constructor function is preferred over a struct literal
-// local value is allocated on heap when its pointer is returned from a function
-// constructor function
-func NewBayan(model string, year int) *Bayan { return &Bayan{model, year} }
-// accessor functions = exported methods for unexported struct fields for
-// data validation, computed values, access serialization
-func (b *Bayan) Model() { return p.model }
-func (b *Bayan) SetModel(model string) { p.model = model }
-// structural typing, anonymous struct
-var a Bayan = struct { model string; year int }{"Nextra", 2022}
-// struct embedding = composition of concrete types
-// embedding makes exported identifiers of an embedded type public
-// multiple types can be embedded into a new type
-// composition = an embedded type is a receiver, a new type is not an embedded type
-// inheritance = a subclass is a receiver and can subsitute a superclass
-type Product struct { Bayan; price float64 } // embedded field without a name
-p := Product{Bayan{"Nextra", 2022}, 2e4}
-// Bayan fields are either directly available on Product through promotion
-// or indirectly accessible through a Product type
-// fields of nested embedded types are promoted to a top-level containing type
-fmt.Println(p.model, p.price, p.Bayan.year)
-// empty struct{}{} value occupies 0 bytes vs
-// empty interface{} type occupies 16 byes
-map[string]struct{} // set of keys
-chan struct{} // signaling channel without data
+// /* Structs, types */
+// // nominal typing, block-level scope
+//   var a, b Bayan = Bayan{model: "Nextra", year: 2022}, Bayan{"Omnia", 2023}
+//   var c, d *Bayan = new(Bayan), &Bayan{"Selecta", 2024}
+//   fmt.Println(d.model) // (*d).model => d.model
+//   e := []Bayan{{model: "Nextra", year: 2022}, {"Omnia", 2023}}
+// // constructor function = correct and uniform initialization of structs
+// // constructor function is preferred over a struct literal
+// // local value is allocated on heap when its pointer is returned from a function
+// // constructor function
+// func NewBayan(model string, year int) *Bayan { return &Bayan{model, year} }
+// // accessor functions = exported methods for unexported struct fields for
+// // data validation, computed values, access serialization
+// func (b *Bayan) Model() { return p.model }
+// func (b *Bayan) SetModel(model string) { p.model = model }
+// // structural typing, anonymous struct
+// var a Bayan = struct { model string; year int }{"Nextra", 2022}
+
+// // struct embedding = composition of concrete types
+// // embedding makes exported identifiers of an embedded type public
+// // multiple types can be embedded into a new type
+// // composition = an embedded type is a receiver, a new type is not an embedded type
+// // inheritance = a subclass is a receiver and can subsitute a superclass
+// type Product struct { Bayan; price float64 } // embedded field without a name
+// p := Product{Bayan{"Nextra", 2022}, 2e4}
+// // Bayan fields are either directly available on Product through promotion
+// // or indirectly accessible through a Product type
+// // fields of nested embedded types are promoted to a top-level containing type
+// fmt.Println(p.model, p.price, p.Bayan.year)
+// // empty struct{}{} value occupies 0 bytes vs
+// // empty interface{} type occupies 16 byes
+// map[string]struct{} // set of keys
+// chan struct{} // signaling channel without data
 
 /* Blocks */
 // package block = definitions outside a function
@@ -123,132 +124,132 @@ chan struct{} // signaling channel without data
 // shadowing = inner block same-name definition, unaccessible outer definition
 // shadowing = re-definition of an identifier in an inner block
 
-/* if/else */
-// allows for arbitrary conditions in each clause
-// each clause has its own scope
-if i := rand.Intn(10); i < 3 { fmt.Println(i, "low")
-} else if i < 8 { fmt.Println(i, "mid")
-} else { fmt.Println(i, "high") }
-// align the happy path to the left; favor early return
-// the happy path goes down, while edge cases are handled on the right
-// prefer vertical if error { return } over nested if/else
+// /* if/else */
+// // allows for arbitrary conditions in each clause
+// // each clause has its own scope
+// if i := rand.Intn(10); i < 3 { fmt.Println(i, "low")
+// } else if i < 8 { fmt.Println(i, "mid")
+// } else { fmt.Println(i, "high") }
+// // align the happy path to the left; favor early return
+// // the happy path goes down, while edge cases are handled on the right
+// // prefer vertical if error { return } over nested if/else
 
-/* for + break/continue [label] */
-// best for controlled iteration defining start, end, and step
-for i := 0; i < 3; i++ { fmt.Println(i) } // 0 1 2
-for i, l := 0, len(a); i < l; i++ { fmt.Println(a[i]) }
-i := 0 // best for dynamic exit condition
-for i < 3 { fmt.Println(i); i++ } // 0 1 2
-i = 0 // unconditional first iteration
-for { fmt.Println(i); i++; if i > 2 { break } } // 0 1 2
-// best for arrays, slices, strings, maps (single container only)
-// for/range returns a copy of values
-for _, v := range []int{1, 2, 3} { fmt.Println(v) } // 1 2 3
-// range expression is a copy evaluated once before a loop
-a := []int{1, 2, 3}
-for range a { a = append(a, 10) } // [1 2 3 10 10 10]
-// all values are assigned to a single variable
-type Acc struct { bal float64 }
-accs := []Acc{{1}, {2}, {3}}
-m := make(map[int]*Acc, len(accs))
-for i, acc := range accs { m[i] = &acc } // 3 3 3
-for i, acc := range accs { a := acc; m[i] = &a } // 1 2 3
-for i := range accs { m[i] = &accs[i] } // 1 2 3
+// /* for + break/continue [label] */
+// // best for controlled iteration defining start, end, and step
+// for i := 0; i < 3; i++ { fmt.Println(i) } // 0 1 2
+// for i, l := 0, len(a); i < l; i++ { fmt.Println(a[i]) }
+// i := 0 // best for dynamic exit condition
+// for i < 3 { fmt.Println(i); i++ } // 0 1 2
+// i = 0 // unconditional first iteration
+// for { fmt.Println(i); i++; if i > 2 { break } } // 0 1 2
+// // best for arrays, slices, strings, maps (single container only)
+// // for/range returns a copy of values
+// for _, v := range []int{1, 2, 3} { fmt.Println(v) } // 1 2 3
+// // range expression is a copy evaluated once before a loop
+// a := []int{1, 2, 3}
+// for range a { a = append(a, 10) } // [1 2 3 10 10 10]
+// // all values are assigned to a single variable
+// type Acc struct { bal float64 }
+// accs := []Acc{{1}, {2}, {3}}
+// m := make(map[int]*Acc, len(accs))
+// for i, acc := range accs { m[i] = &acc } // 3 3 3
+// for i, acc := range accs { a := acc; m[i] = &a } // 1 2 3
+// for i := range accs { m[i] = &accs[i] } // 1 2 3
 
-/* switch + break [label] */
-a := []string{"one", "eleven", "thousand"}
-outer: for _, v := range a {
-  switch l := len(v); l { // equality check == in each clause
-  case 1, 2, 3: fmt.Println(v, "small")
-  // use break label in a switch within a for loop
-  case 4, 5, 6: fmt.Println(v, "medium"); break outer
-  default: fmt.Println(v, "large")
-  }
-}
-for _, v := range a {
-  switch l := len(v); { // arbitrary conditions in clause clause
-  case l < 4: fmt.Println(v, "small")
-  case l < 7: fmt.Println(v, "medium")
-  default: fmt.Println(v, "large")
-  }
-}
+// /* switch + break [label] */
+// a := []string{"one", "eleven", "thousand"}
+// outer: for _, v := range a {
+//   switch l := len(v); l { // equality check == in each clause
+//   case 1, 2, 3: fmt.Println(v, "small")
+//   // use break label in a switch within a for loop
+//   case 4, 5, 6: fmt.Println(v, "medium"); break outer
+//   default: fmt.Println(v, "large")
+//   }
+// }
+// for _, v := range a {
+//   switch l := len(v); { // arbitrary conditions in clause clause
+//   case l < 4: fmt.Println(v, "small")
+//   case l < 7: fmt.Println(v, "medium")
+//   default: fmt.Println(v, "large")
+//   }
+// }
 
-/* goto label */
-// prefer goto over flow control flags or code duplication
-for _, v := range []int{1, 2, 3} {
-  if v == 2 { goto print }
-  v *= 10
-  print: fmt.Println(v) // 10, 2, 30
-}
+// /* goto label */
+// // prefer goto over flow control flags or code duplication
+// for _, v := range []int{1, 2, 3} {
+//   if v == 2 { goto print }
+//   v *= 10
+//   print: fmt.Println(v) // 10, 2, 30
+// }
 
-/* functions */
-// call-by-value = all function arguments are copies, no need for immutability
-// primitive and composite types are value types, slices and maps are pointers
-// named return values = for documentation (interface), initialization, defer
-// avoid naked returns for clarity
-func quoteRem(a, b int) (quote, rem int) {
-  quote, rem = a / b, a % b; return quote, rem
-}
-// variadic parameters
-func sum(args ...int) (sum int) {
-  for _, arg := range args { sum += arg }; return sum
-}
-fmt.Println(sum([]int{1, 2, 3}...))
-// function type, functions are values, block-level scope
-type Op func(int, int) int
-ops := map[string]Op{
-  "+": func(a, b int) int { return a + b },
-  "-": func(a, b int) int { return a - b },
-}
-// anonymous functions (function literals) are closures
-// closed on variables are evaluated every time when a closure is invoked
-sort.Slice(bayans, func(i, j int) bool {
-  return bayans[i].model < bayans[j].model
-})
+// /* functions */
+// // call-by-value = all function arguments are copies, no need for immutability
+// // primitive and composite types are value types, slices and maps are pointers
+// // named return values = for documentation (interface), initialization, defer
+// // avoid naked returns for clarity
+// func quoteRem(a, b int) (quote, rem int) {
+//   quote, rem = a / b, a % b; return quote, rem
+// }
+// // variadic parameters
+// func sum(args ...int) (sum int) {
+//   for _, arg := range args { sum += arg }; return sum
+// }
+// fmt.Println(sum([]int{1, 2, 3}...))
+// // function type, functions are values, block-level scope
+// type Op func(int, int) int
+// ops := map[string]Op{
+//   "+": func(a, b int) int { return a + b },
+//   "-": func(a, b int) int { return a - b },
+// }
+// // anonymous functions (function literals) are closures
+// // closed on variables are evaluated every time when a closure is invoked
+// sort.Slice(bayans, func(i, j int) bool {
+//   return bayans[i].model < bayans[j].model
+// })
 
-/* defer */
-// defer closures are evaluated after function return in the reverse order
-// receivers are evaluated when declared, not when executed
-defer f.Close()
-defer func() {
-  if err == nil { err = tx.Commit() } else { tx.Rollback() }
-}() // defer must end with ()
-// arguments to defer closures are evaluated when declared, not when executed
-var i, j int
-defer func(i int) { fmt.Println(i, j) }(i) // 0 1
-i++; j++
-// capture and return an error from defer; must use a named return + closure
-func f() (err error) {
-  err = fmt.Errorf("function oh")
-  defer func() {
-    // wrap a function error in a defer error
-    if err != nil { err = fmt.Errorf("defer oh: %w", err)
-    } else { err = fmt.Errorf("defer oh") }
-  }()
-  return err
-}
+// /* defer */
+// // defer closures are evaluated after function return in the reverse order
+// // receivers are evaluated when declared, not when executed
+// defer f.Close()
+// defer func() {
+//   if err == nil { err = tx.Commit() } else { tx.Rollback() }
+// }() // defer must end with ()
+// // arguments to defer closures are evaluated when declared, not when executed
+// var i, j int
+// defer func(i int) { fmt.Println(i, j) }(i) // 0 1
+// i++; j++
+// // capture and return an error from defer; must use a named return + closure
+// func f() (err error) {
+//   err = fmt.Errorf("function oh")
+//   defer func() {
+//     // wrap a function error in a defer error
+//     if err != nil { err = fmt.Errorf("defer oh: %w", err)
+//     } else { err = fmt.Errorf("defer oh") }
+//   }()
+//   return err
+// }
 
-/* methods */
-// method = a function that operates on a type pointer or a type value
-// function(receiver, args...) => receiver.method(args...)
-// method modifies a receiver => must use a pointer receiver (p *T), large
-// objects
-// method does not modify a receiver => may use a value receiver (v T),
-// primitives. Value receiver copies a type when a method is invoked
-// value receiver = a copy of a receiver is passed to a method
-// method can be invoked through a nil pointer/receiver = valid receiver
-// mixing receiver types should be avoided
-type Bayan struct { model string; year int }
-func (b *Bayan) show() { fmt.Printf("Bayan{%s %d}\n", b.model, b.year) }
-var b = Bayan{"Nextra", 2022}; var p = &b
-b.show(); p.show() // b.show() => (&b).show()
-bShow := b.show; bShow() // method value = closure over its instance
-BShow := (*Bayan).show; BShow(&b) // method expression => function(receiver)
-// struct embedding = composition of concrete types
-// fields and methods of an embedded type are promoted to a containing type
-type Product struct { Bayan; price float64 }
-p := Product{Bayan{"Nextra", 2023}, 1e3}
-fmt.Println(p.model); p.show()
+// /* methods */
+// // method = a function that operates on a type pointer or a type value
+// // function(receiver, args...) => receiver.method(args...)
+// // method modifies a receiver => must use a pointer receiver (p *T), large
+// // objects
+// // method does not modify a receiver => may use a value receiver (v T),
+// // primitives. Value receiver copies a type when a method is invoked
+// // value receiver = a copy of a receiver is passed to a method
+// // method can be invoked through a nil pointer/receiver = valid receiver
+// // mixing receiver types should be avoided
+// type Bayan struct { model string; year int }
+// func (b *Bayan) show() { fmt.Printf("Bayan{%s %d}\n", b.model, b.year) }
+// var b = Bayan{"Nextra", 2022}; var p = &b
+// b.show(); p.show() // b.show() => (&b).show()
+// bShow := b.show; bShow() // method value = closure over its instance
+// BShow := (*Bayan).show; BShow(&b) // method expression => function(receiver)
+// // struct embedding = composition of concrete types
+// // fields and methods of an embedded type are promoted to a containing type
+// type Product struct { Bayan; price float64 }
+// p := Product{Bayan{"Nextra", 2023}, 1e3}
+// fmt.Println(p.model); p.show()
 
 /* interfaces */
 // interface (the only abstract type)
